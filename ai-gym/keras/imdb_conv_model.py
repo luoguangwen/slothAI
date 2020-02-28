44d1899ff62020717719482f6b1045c976c2420d
KevinLuo <luoguangwen@163.com>
2020-02-28 22:38:02 +0800

    imdb dataset convolution model

---
+++ ai-gym/keras/imdb_conv_model.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   imdb_conv_model.py   
+@Contact :   luoguangwen@163.com
+@Create Time: 2020/2/28 22:30      
+@Author  : Kevin.Luo  
+@Version : 1.0 
+@Desciption :
+"""
+
+
+def main():
+    print("this main function entry")
+    pass
+
+
+if __name__ == '__main__':
+    main()
+    pass
